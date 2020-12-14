a92f5d3fbf578443366646f2a03b5371e76ccd3a
LAPTOP-ND5NS0AC\Alena <bobrova555@gmail.com>
2020-12-14 18:11:55 +0300

    fix production build

--- components/TestLayout/TestLayout.tsx
+++ components/TestLayout/TestLayout.tsx
@@ -2,6 +2,10 @@ import styles from './TestLayout.module.scss';
 import Button from 'react-bootstrap/Button'
 import React from 'react';
 
+interface ITestLayoutState{
+    url: URL
+}
+
 export default class TestLayout extends React.Component {
     constructor(props) {
         super(props);
@@ -11,15 +15,18 @@ export default class TestLayout extends React.Component {
         globalThis.addEventListener('message', (event) => this.onMessage(event));
     }
     onMessage(event){
-        if(event.origin === this.state.url?.origin)
+        let state = this.state as ITestLayoutState;
+        if(event.origin === state.url?.origin)
             console.log(`Links: ${event.data}`);
     }
     render(){
         let checkLinks = () => {
-            if(this.state.url)
-                globalThis.pageViewer.contentWindow.postMessage('links', this.state.url);
+            let state = this.state as ITestLayoutState;
+            if(state.url)
+                globalThis.pageViewer.contentWindow.postMessage('links', state.url);
         }
         
+        let state = this.state as ITestLayoutState;
         let selectFileStr = 'Select a file';
         let checkStr='Check links';
 
@@ -34,7 +41,7 @@ export default class TestLayout extends React.Component {
                     </div>
                 </div>
                 <div className={styles.stretched}>
-                    <iframe className={styles.htmlViewer} id="pageViewer" src={this.state.url}></iframe>
+                    <iframe className={styles.htmlViewer} id="pageViewer" src={state.url?.toString()}></iframe>
                 </div>
             </div>
         )
